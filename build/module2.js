c1a438a33307a3664be44d05172dc7361d4d38db
Anwar Hossain <irishkhan33@gmail.com>
2023-06-27 13:19:15 +0600

    function type checking module practice added

--- build/module2.js
+++ build/module2.js
@@ -1,5 +1,14 @@
 "use strict";
 //interface
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let myIceCream = {
     flavor: 'vanilla',
     scoops: 2,
@@ -20,3 +29,25 @@ let myIceCream2;
 myIceCream2 = ['5', 'vanilla', 'strawberry'];
 let myStr = myIceCream2[2];
 console.log(myStr);
+// JavaScript API using an interface
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts';
+function fetchPosts(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response = yield fetch(url);
+        let body = yield response.json();
+        return body;
+    });
+}
+function showPost() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let posts = yield fetchPosts(fetchURL);
+        // Display the contents of the first item in the response
+        let post = posts[0];
+        console.log('Post #' + post.id);
+        // If the userId is 1, then display a note that it's an administrator
+        console.log('Author: ' + (post.userId === 1 ? "Administrator" : post.userId.toString()));
+        console.log('Title: ' + post.title);
+        console.log('Body: ' + post.body);
+    });
+}
+showPost();
